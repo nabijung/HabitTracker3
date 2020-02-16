dd7c752aa56ed0ba9649990088141c38d923a2dc
Nabi Jung <nabijung@icloud.com>
2020-02-16 02:04:18 -0500

    finished Assignment

---
+++ TaskCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TaskCollectionViewCell.swift
+//  HabitTracker
+//
+//  Created by nabi jung on 2/12/20.
+//
+
+import UIKit
+
+class TaskCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ TaskTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskTableViewCell.swift
+//  HabitTracker
+//
+//  Created by nabi jung on 2/12/20.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
